0f4b2ce5288cacd0770c2ba826fa8d0ec8978943
4drvim61 <4drivm61@solent.ac.uk>
2021-11-02 21:38:53 +0000

    currency converter complete

---
+++ currency_converter/Pages/index.cshtml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace currency_converter.Pages
+{
+    public class currencyconverterModel : PageModel
+    {
+        private readonly ILogger<currencyconverterModel> _logger;
+
+        [BindProperty]
+        public String FullCurr { get; set; }
+
+        [BindProperty]
+        public String currency { get; set; }
+
+        [BindProperty]
+        public double amount { get; set; }
+
+        public currencyconverterModel(ILogger<currencyconverterModel> logger)
+        {
+            _logger = logger;
+        }
+        public void OnGet()
+        {
+
+        }
+        public void OnPost()
+        {
+            if (currency == "Pounds")
+                {
+                amount = amount * 1.8;
+
+                string x = amount.ToString();
+
+                @ViewData["Result"] = "€" + x;
+
+            }
+            else if (currency == "Euros")
+            {
+
+                amount = amount * 0.8;
+
+                string x = amount.ToString();
+
+                @ViewData["Result"] = "£" + x;
+
+            }
+            else
+            {
+                @ViewData["Result"] = "Failed";
+            }
+        }
+    }
+}
---
+++ name_input/Pages/Class.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace name_input.Pages
+{
+    public class Class
+    {
+    }
+}
